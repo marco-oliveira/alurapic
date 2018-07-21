999bf046b1caf51d086995f757ed4cbbc6e108d6
marco-oliveira <marco-oliveira@live.com>
2018-07-20 22:00:48 -0300

    Criando component photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  photos = [
+    {
+      url: 'https://cdn.pixabay.com/photo/2013/04/01/11/00/stop-98913_960_720.png',
+      description: 'pare'
+    },
+    {
+      url: 'https://cdn.pixabay.com/photo/2013/04/19/19/28/cantina-105827_960_720.png',
+      description: 'pare'
+    },
+    {
+      url: 'https://cdn.pixabay.com/photo/2013/04/19/19/28/cantina-105827_960_720.png',
+      description: 'pare'
+    }
+  ];
 }
